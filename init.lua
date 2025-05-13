a0005d2c59880cfba53612b41401c1f870bb698c
Eric Edmond <e_edmond@apple.com>
2025-05-12 22:14:02 -0700

    Add xcodebuild plugin and catpuccin theme

--- init.lua
+++ init.lua
@@ -116,5 +116,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- setup must be called before loading
+vim.cmd.colorscheme 'catppuccin'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
