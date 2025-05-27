410dd0df151d2c20f26ec161e094e30d936111f1
Eric Edmond <e_edmond@apple.com>
2025-05-27 10:08:52 -0700

    Move branch to right side

--- lua/custom/plugins/lualine-themes/bubbles.lua
+++ lua/custom/plugins/lualine-themes/bubbles.lua
@@ -39,12 +39,12 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { { 'filename', path = 1 }, 'branch' },
+    lualine_b = { { 'filename', path = 1 } },
     lualine_c = {
       '%=', --[[ add your center components here in place of this comment ]]
     },
     lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
+    lualine_y = { 'branch', 'filetype', 'progress' },
     lualine_z = {
       { 'location', separator = { right = '' }, left_padding = 2 },
     },
