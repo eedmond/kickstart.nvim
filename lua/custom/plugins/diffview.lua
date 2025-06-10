d7babac3bf7fa2ece6d7baa70f78ca9810ad9c76
Eric Edmond <e_edmond@apple.com>
2025-06-10 10:37:29 -0700

    Import diffview with 4-way view for merge conflicts

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    name = 'diffview',
+    opts = {
+      view = {
+        merge_tool = {
+          layout = 'diff4_mixed',
+        },
+      },
+    },
+  },
+}
