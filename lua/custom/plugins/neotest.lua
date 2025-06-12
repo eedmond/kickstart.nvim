bafa9888f64c7fbb31c6b484c86120e172624248
Eric Edmond <ericed3022@gmail.com>
2025-06-11 18:41:06 -0700

    Get neotest working with playwright and keymapping for run nearest test

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,27 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'thenbe/neotest-playwright',
+    dependencies = 'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require('neotest-playwright').adapter {
+          options = {
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
+          },
+        },
+      },
+    }
+  end,
+  keys = {
+    {
+      '<leader>tr',
+      "<cmd>lua require('neotest').run.run()<CR>",
+      desc = '[T]est [R]un',
+    },
+  },
+  cmd = 'Neotest',
+}
