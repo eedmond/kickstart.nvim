36e49f12a349ab43121aa7b40b091df26559372b
Eric Edmond <e_edmond@apple.com>
2025-09-12 14:57:37 -0700

    Get diffview visuals and shortcuts working nicely.
    Still need to be able to save file (?) Not sure why that isn't working.
    
    I also want shortcuts for diffing the current file against HEAD and things like that, but this is a nice start.

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -15,50 +15,35 @@ return {
     },
     config = function(_, opts)
       vim.opt.fillchars = {
+        foldopen = '',
+        foldclose = '',
         fold = ' ',
-        diff = '╱',
-        wbr = '─',
-        msgsep = '─',
-        horiz = ' ',
-        horizup = '│',
-        horizdown = '│',
-        vertright = '│',
-        vertleft = '│',
-        verthoriz = '│',
+        foldsep = ' ',
+        eob = ' ', -- Don't show ~ at end of buffer
+        diff = '╱', -- Nicer delete lines in DiffView
       }
 
-      local colors = {
-        '#40a02b', -- 0 Green
-        '#d20f39', -- 1 Red
-        '#1e66f5', -- 2 Blue
-        '#179299', -- 3 Teal
-        '#fe640b', -- 4 Peach
-        '#df8e1d', -- 5 Yellow
-        '#8839ef', -- 6 Mauve
-        '#40a02b', -- 7 Green
-        '#ea76cb', -- 8 Pink
-        '#4c4f69', -- 9 Text
-        '#1e1e2d', -- 10 Base
+      vim.o.diffopt = 'internal,filler,closeoff,linematch:40'
+
+      local actions = require 'diffview.actions'
+
+      opts.keymaps = {
+        view = {
+          { 'n', '[x', actions.prev_conflict, { desc = 'In the merge-tool: jump to the previous conflict' } },
+          { 'n', ']x', actions.next_conflict, { desc = 'In the merge-tool: jump to the next conflict' } },
+          { 'n', '<leader>co', actions.conflict_choose 'ours', { desc = 'Choose the OURS version of a conflict' } },
+          { 'n', '<leader>ct', actions.conflict_choose 'theirs', { desc = 'Choose the THEIRS version of a conflict' } },
+          { 'n', '<leader>cb', actions.conflict_choose 'base', { desc = 'Choose the BASE version of a conflict' } },
+          { 'n', '<leader>ca', actions.conflict_choose 'all', { desc = 'Choose all the versions of a conflict' } },
+          { 'n', 'dx', actions.conflict_choose 'none', { desc = 'Delete the conflict region' } },
+          { 'n', '<leader>cO', actions.conflict_choose_all 'ours', { desc = 'Choose the OURS version of a conflict for the whole file' } },
+          { 'n', '<leader>cT', actions.conflict_choose_all 'theirs', { desc = 'Choose the THEIRS version of a conflict for the whole file' } },
+          { 'n', '<leader>cB', actions.conflict_choose_all 'base', { desc = 'Choose the BASE version of a conflict for the whole file' } },
+          { 'n', '<leader>cA', actions.conflict_choose_all 'all', { desc = 'Choose all the versions of a conflict for the whole file' } },
+          { 'n', 'dX', actions.conflict_choose_all 'none', { desc = 'Delete the conflict region for the whole file' } },
+        },
       }
-      vim.api.nvim_set_hl(0, 'DiffAdd', { fg = colors[0], bg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'DiffDelete', { fg = colors[9], bg = colors[10] })
-      vim.api.nvim_set_hl(0, 'DiffChange', { fg = colors[2], bg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'DiffText', { fg = colors[3], bg = 'NONE' })
 
-      vim.api.nvim_set_hl(0, 'DiffAdded', { fg = colors[0], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffRemoved', { fg = colors[1], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffChanged', { fg = colors[2], bg = 'NONE', bold = true })
-
-      vim.api.nvim_set_hl(0, 'DiffviewWinSeparator', { fg = colors[9], bg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { fg = colors[9], bg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'DiffviewFilePanelSelected', { fg = colors[6], bg = 'NONE' })
-
-      vim.api.nvim_set_hl(0, 'DiffviewStatusAdded', { fg = colors[0], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffviewStatusUntracked', { fg = colors[8], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffviewStatusModified', { fg = colors[4], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffviewStatusRenamed', { fg = colors[3], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffviewStatusDeleted', { fg = colors[1], bg = 'NONE', bold = true })
-      vim.api.nvim_set_hl(0, 'DiffviewStatusIgnored', { fg = colors[9], bg = 'NONE', bold = true })
       require('diffview').setup(opts)
     end,
   },
