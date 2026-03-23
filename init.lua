4afa7303a9868d824ec7f8243b6c0226b8963357
Eric Edmond <e_edmond@apple.com>
2026-03-23 11:38:09 -0700

    Improve startup time (Claude assisted)

--- init.lua
+++ init.lua
@@ -114,12 +114,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
-require 'diffview'
-
 require 'custom.plugins.catppuccin'
 vim.cmd.colorscheme 'catppuccin'
-
-require 'custom.plugins.lualine-themes.bubbles'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
