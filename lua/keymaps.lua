10ddad008fc978dc0817aa85400a45644bae1e62
Eric Edmond <e_edmond@apple.com>
2026-04-13 12:51:45 -0700

    Add <leader>+ml shorcut to list marks

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Marks
+vim.keymap.set('n', '<leader>ml', '<cmd>marks<CR>', { desc = '[M]arks [L]ist' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
