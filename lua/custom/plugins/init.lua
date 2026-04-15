60b2b47cd37944cc3e954292b679205a345d3f27
Eric Edmond <e_edmond@apple.com>
2026-04-15 12:00:53 -0700

    Update plugins, remove xcodebuild, fix blink-cmp

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,23 +11,6 @@ return {
       --Config goes here
     },
   },
-  {
-    'wojciech-kulik/xcodebuild.nvim',
-    ft = 'swift',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'MunifTanjim/nui.nvim',
-      -- 'folke/snacks.nvim', -- (optional) to show previews
-      'nvim-tree/nvim-tree.lua', -- (optional) to manage project files
-      'stevearc/oil.nvim', -- (optional) to manage project files
-      'nvim-treesitter/nvim-treesitter', -- (optional) for Quick tests support (required Swift parser)
-    },
-    config = function()
-      require('xcodebuild').setup {
-        -- put some options here or leave it empty to use default settings
-      }
-    end,
-  },
   { 'catppuccin/nvim', name = 'catppuccin', flavour = 'mocha', priority = 1000 },
   {
     'iamcco/markdown-preview.nvim',
